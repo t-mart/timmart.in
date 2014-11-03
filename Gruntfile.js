eace0c1218cc53687a53a6eaacc1ed415d1acc85
Tim Martin <tim@timmart.in>
2014-11-03 02:34:23 -0500

    move to superior jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,105 +4,20 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    devConf: 'pelicanconf.py',
-    prodConf: 'publishconf.py',
-    hamlContentDir: 'content/haml',
-    contentDir: 'content/html',
-    outputDir: 'output',
-    themeDir: 'timmart.in_theme',
-
-    watch: {
-      output: {
-        files: ['<%= outputDir %>/**/*'],
-        options: {
-          livereload: true
-        }
-      },
-      pelicanDev: {
-        files: ['<%= contentDir %>/**/*', '<%= themeDir %>/**/*', '<%= devConf %>'],
-        tasks: 'shell:pelicanDev'
-      },
-      pelicanProd: {
-        files: ['<%= contentDir %>/**/*', '<%= themeDir %>/**/*', '<%= prodConf %>'],
-        tasks: 'shell:pelicanProd'
-      },
-      haml: {
-        files: ['<%= hamlContentDir %>/*.haml'],
-        tasks: 'haml:articles'
-      }
-    },
-
-    haml: {
-      description: "Run articles thru haml",
-      articles: {
-        files: [ {
-          expand: true,
-          cwd: '<%= hamlContentDir %>',
-          src: ['*.haml'],
-          dest: '<%= contentDir %>',
-          ext: '.html'
-        } ]
-      }
-    },
-
-    connect: {
-      server: {
-        options: {
-          livereload: true,
-          keepalive: true,
-          base: '<%= outputDir %>'
-        }
-      }
-    },
-
+    outputDir: '_site',
     shell: {
-      pelicanDev: {
-        command: 'pelican <%= contentDir %> -o <%= outputDir %> -s <%= devConf %>'
-      },
-      pelicanProd: {
-        command: 'pelican <%= contentDir %> -o <%= outputDir %> -s <%= prodConf %>'
-      },
       ghpimport: {
         command: function() {
           var isoDate = new Date().toISOString();
           return 'ghp-import -p -m "update ' + isoDate + '" <%= outputDir %>';
         }
       },
-    },
-
-    concurrent: {
-      options: {
-        logConcurrentOutput: true,
-        limit: 4
-      },
-      serveDev: {
-        tasks: ['watch:pelicanDev', 'watch:output', 'watch:haml', 'connect:server']
-      },
-      serveProd: {
-        tasks: ['watch:pelicanProd', 'watch:output', 'watch:haml', 'connect:server']
-      },
-    },
-
-    clean: ['__pycache__', '<%= outputDir %>', 'cache']
-
+    }
   });
 
   // plugin tasks
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-haml2html');
 
   // my tasks
-  grunt.registerTask('dev-serve', 'Start a server that watches for content changes and runs pelican with development settings', ['shell:pelicanDev', 'concurrent:serveDev']);
-  grunt.registerTask('prod-serve', 'Like dev-serve, but run with production settings', ['shell:pelicanProd', 'concurrent:serveProd']);
-
-  grunt.registerTask('pelican-dev', 'Run pelican with development settings', ['shell:pelicanDev']);
-  grunt.registerTask('pelican-prod', 'Run pelican with production settings', ['shell:pelicanProd']);
-
   grunt.registerTask('publish', 'Push a production pelican build to github for WWW serving', ['shell:pelicanProd', 'shell:ghpimport']);
-
-  grunt.registerTask('default', 'Alias for dev-serve', ['dev-serve']);
 };
---
+++ _posts/2014-11-02-hello_world.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "Hello World!"
+date:   2014-11-02 12:06:08
+---
+
+Welcome to my blog!
